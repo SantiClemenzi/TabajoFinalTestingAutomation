11918b7d7517c4bd943299a0766f8ef04f31104c
SantiClemenzi <saclemenzi@gmail.com>
2026-09-16 17:30:49 -0300

    creamos metodos en el BasePage y agrego plugins en el pom.xml

--- src/main/java/ar/org/icaro/pages/BasePage.java
+++ src/main/java/ar/org/icaro/pages/BasePage.java
@@ -1,4 +1,72 @@
 package ar.org.icaro.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    //Metodo de Click
+    protected void click(By locator) {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        element.click();
+    }
+
+    //Metodo para tipear
+    protected void type(By locator, String text) {
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    //Metodo para obtener texto
+    protected String getText(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).getText();
+    }
+
+    //Metodo para saber si un elemento es visible
+    protected Boolean isElementVisible(By locator) {
+        try {
+            //Espera a que el elemento aparezca
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            return true;
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    //Metodo para saber hay en la url
+    protected boolean waitForUrlContains(String text) {
+        try {
+            wait.until(ExpectedConditions.urlContains(text));
+            return true;
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    //Metodo para obtener url actual
+    protected String getCurrentUrl(By locator) {
+        return driver.getCurrentUrl();
+    }
+
+    //Espera a que el elemento desaparezca
+    protected boolean waitForElementToDisappear(By locator) {
+        try {
+            return wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
 }

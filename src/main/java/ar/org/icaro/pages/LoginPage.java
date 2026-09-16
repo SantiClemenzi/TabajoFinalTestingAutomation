523a16fc34185c81cf230bd9b5a2a52f7e9cfd95
SantiClemenzi <saclemenzi@gmail.com>
2026-09-16 17:54:33 -0300

    creamos los metodos de LoginPage

--- src/main/java/ar/org/icaro/pages/LoginPage.java
+++ src/main/java/ar/org/icaro/pages/LoginPage.java
@@ -1,4 +1,55 @@
 package ar.org.icaro.pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+
+public class LoginPage extends BasePage {
+    private By usernameField = By.id("username");
+    private By passwordField = By.id("password");
+    private By loginButton = By.id("login");
+    private By errorMessage = By.className("oxd-alert");
+    private static final String URL_BASE = "https://opensource-demo.orangehrmlive.com/";
+
+    //Constructor
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+    public LoginPage goTo(){
+        driver.get(URL_BASE);
+        return this;
+    }
+
+    public LoginPage enterUsername(String username) {
+        type(usernameField, username);
+        return this;
+    }
+
+    public LoginPage enterPassword(String password) {
+        type(passwordField, password);
+        return this;
+    }
+
+    public InventoryPage clickLogin() {
+        click(loginButton);
+        return new InventoryPage(driver);
+    }
+
+    public InventoryPage loginAs(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        return clickLogin();
+    }
+
+    public boolean isErrorDisplayed(){
+        return isElementVisible(errorMessage);
+    }
+
+    public boolean isOnLoginPage() {
+        //Validamos mediante la URL esperada
+        return waitForUrlContains("opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
 }

e37da8238a65b9146ea7582e553928a92bf97f5f
SantiClemenzi <saclemenzi@gmail.com>
2026-09-16 18:55:26 -0300

    primer test en FlujoTest y modificacion de dependencias en el POM.XML

--- src/test/java/ar/org/icaro/test/FlujoCompletoTest.java
+++ src/test/java/ar/org/icaro/test/FlujoCompletoTest.java
@@ -1,4 +1,47 @@
 package ar.org.icaro.test;
 
+import ar.org.icaro.pages.DashboardPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 public class FlujoCompletoTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private LoginPage  loginPage;
+
+
+    @BeforeMethod
+    public void setUp() {
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // SOLUCIÓN: Instanciamos la página pasándole el driver recién creado
+        loginPage = new LoginPage(driver);
+    }
+
+    @AfterMethod
+    public void TearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test(priority=1, description="FLUJO COMPLETO")
+    public void completoTest() {
+        System.out.println("1: Vamos al Login");
+        DashboardPage dashboard = loginPage.goTo().loginAs("Admin","admin123");
+        Assert.assertTrue(dashboard.isOnDashboardPage(), "deberia estar en el Dashboard");
+    }
 }
